ba92861fad58ad43db1af4e7da0e8800ce3818e1
Cherica <riantsoacherica01@gmail.com>
2026-06-29 12:12:54 +0300

    ma

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,5 +1,5 @@
 "use client";import React, { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 import { Button } from "@/components/ui/button";
 import {
   Card,
